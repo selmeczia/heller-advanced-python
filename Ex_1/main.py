e85c1439ecc7a42496fce6d8824c657cc39a9daf
Adam <selmeczi.toth.adam@gmail.com>
2021-02-12 08:49:41 +0100

    added comment for every line

--- Ex_1/main.py
+++ Ex_1/main.py
@@ -1,5 +1,9 @@
+# creating numbers into list from 1 to 100
 numbers = list(range(1, 100))
+# creating empty list
 num_3 = list()
+# for cycle for every number divisible by 3
 for num in numbers:
     if num % 3 == 0: num_3.append(num)
+# print return
 print(num_3)
